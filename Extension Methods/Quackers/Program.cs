6de06b8191ba17af42e5baa05117ccfecae29d7d
Rlong14 <rlong14@nait.ca>
2017-09-07 14:46:23 -0600

    Freak out my friends :+1:

---
+++ Extension Methods/Quackers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quackers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Rebecca Long";
+            Console.WriteLine(name);
+            Console.WriteLine(name.Quack());
+        }
+    }
+
+    public static class Extensions // NOTE that this is STATIC
+    {
+        public static string  Quack (this string self)
+        {
+            return self + " (quack)";
+        }
+    }
+}
